49b13a6a687d2b089bc0d83dab59058df02c8121
tranchi0120 <tranchi20012000@gmail.com>
2023-04-08 21:32:19 +0700

    done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const menu = document.querySelector(".menu");
-
 const OpenMenu = document.querySelector(".menu-mobile");
 const CLoseMenu = document.querySelector(".menu-close");
 
@@ -11,6 +10,16 @@ CLoseMenu.addEventListener("click", () => {
   menu.classList.remove("mobilemenu");
 });
 
+/*==================== ScrollTop ====================*/
+
+const scrollTop = () => {
+  const scrollTop = document.getElementById("scroll-top");
+  if (window.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollTop);
+
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr__top = ScrollReveal({
   origin: "top",
@@ -20,8 +29,8 @@ const sr__top = ScrollReveal({
 });
 
 sr__top.reveal(
-  `.hero,.about-top,.about-box,.title,.product-bottom-box ,.product-box,
-  .benefit-box,.view-image,.testimonial-box,.hero-btn__primary,.product-bottom__btn,.connect-title,
+  `.hero,.about-top,.about-box,.title,.product-bottom-box ,.product-group,
+  .benefit-box,.view-image,.testimonial-box,.hero-btn__primary,.connect-title,
   .hero-btn__second,.connect-form,.blog-box,.footer-top`,
   {
     interval: 200,
